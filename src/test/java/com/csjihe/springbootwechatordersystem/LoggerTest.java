7ee233e5831dd6665c1a94ed5d1190a0788e7a02
spmonkey <csjihe@gmail.com>
2018-12-30 11:31:57 -0500

    Logging config finished

--- src/test/java/com/csjihe/springbootwechatordersystem/LoggerTest.java
+++ src/test/java/com/csjihe/springbootwechatordersystem/LoggerTest.java
@@ -1,5 +1,6 @@
 package com.csjihe.springbootwechatordersystem;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
@@ -10,18 +11,18 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Slf4j
 public class LoggerTest {
 
-    private final Logger logger = LoggerFactory.getLogger(LoggerTest.class);
 
     @Test
     public void test1() {
         String name = "imooc";
         String password = "123456";
-        logger.debug("debug...");
-        logger.info("name: " + name + ", password: " + password);
-        logger.info("name: {}, password: {}", name, password);
-        logger.error("error...");
+        log.debug("debug...");
+        log.info("name: " + name + ", password: " + password);
+        log.info("name: {}, password: {}", name, password);
+        log.error("error...");
 
     }
 }
